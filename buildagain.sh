1008146f78e66d8560910d01f7d8a249d7bad5f4
vamsics2 <39087062+vamsics2@users.noreply.github.com>
2021-07-08 01:29:38 +0530

    Update buildagain.sh

--- buildagain.sh
+++ buildagain.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
 cd /home/ec2-user/dockerized/
 sudo docker build -t simple-nginx .
 sudo docker run -d -p 80:80 simple-nginx
