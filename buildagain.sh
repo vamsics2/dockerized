7c9334d8cecdaf85ba474fa747acbcf205f3eb2f
vamsics2 <39087062+vamsics2@users.noreply.github.com>
2021-07-08 11:24:20 +0530

    Update buildagain.sh

--- buildagain.sh
+++ buildagain.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/dockerized/
-docker rm -f $(docker ps -q) || docker build -t simple-nginx . && docker run -d -p 80:80 simple-nginx
+sudo docker rm -f $(docker ps -q) || sudo docker build -t simple-nginx . && sudo docker run -d -p 80:80 simple-nginx
