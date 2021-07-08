f4f65f96f69fe6e592e41f262ddc70099da00987
vamsics2 <39087062+vamsics2@users.noreply.github.com>
2021-07-08 09:21:15 +0530

    Update buildagain.sh

--- buildagain.sh
+++ buildagain.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io
 cd /home/ec2-user/dockerized/
 sudo docker build -t simple-nginx .
 sudo docker run -d -p 80:80 simple-nginx
